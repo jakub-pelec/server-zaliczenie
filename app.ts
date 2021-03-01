eee31596c6550adecb5c8d874ad9a6808bce88b8
jpelec <j.pelec@cctechnology.pl>
2021-03-01 11:33:11 +0100

    INITIAL COMMIT

---
+++ app.ts
@@ -0,0 +1,37 @@
+import express, {Application, Request, Response} from 'express';
+import {getBalance, createAccount} from './actions';
+const PORT: number = 5000;
+
+const app: Application = express();
+
+app.listen(PORT, (): void => {
+	console.log(`server listening on port ${PORT}`);
+})
+
+app.post('/create_account', async(request: Request, response: Response) => {
+	const {body: {balance, accountNumber, name, email}} = request;
+	if(!balance || !accountNumber || !name || !email) {
+		return response.status(403).send('Missing parameters.')
+	}
+	try {
+		const id = await createAccount(name, email, accountNumber, balance);
+		return response.status(200).send(id);
+	} catch(e) {
+		return response.status(500).send('Something went wrong.');
+	}
+})
+
+app.post('/get_balance', async(request: Request, response: Response) => {
+	const {body: {id}} = request;
+	if(!id) {
+		return response.status(403).send('Missing parameter.');
+	}
+	const collection: string = 'users';
+	try {
+		const data = await getBalance(collection, id);
+		return response.status(200).send(data);
+	} catch(e) {
+		return response.status(500).send('Something went wrong.');
+	}
+
+}) 
