f237759f26e9dc572a29d9cbf55db9f75795a898
jpelec <j.pelec@cctechnology.pl>
2021-03-02 20:26:28 +0100

    a

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyDYkwrsInkHG8QDPXvcxk-RrYopgg00xiM",
+	authDomain: "serwer-zaliczenie.firebaseapp.com",
+	projectId: "serwer-zaliczenie",
+	storageBucket: "serwer-zaliczenie.appspot.com",
+	messagingSenderId: "761901257432",
+	appId: "1:761901257432:web:c95b516a33f901909bbdad"
+  };
