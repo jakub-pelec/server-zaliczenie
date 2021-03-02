8b0da7acaf4c3af7f7a4489623fa5df4bb8b0d13
jpelec <j.pelec@cctechnology.pl>
2021-03-02 20:23:56 +0100

    a

--- src/actions.ts
+++ src/actions.ts
@@ -1,5 +1,5 @@
 import firebase from 'firebase';
-import {firebaseConfig} from '../firebaseConfig';
+import {firebaseConfig} from './firebaseConfig';
 
 firebase.initializeApp(firebaseConfig);
 const COLLECTION_USERS: string = 'users';
