571107e457ed8fb40f11301668ddb1e9e6d26064
jpelec <j.pelec@cctechnology.pl>
2021-03-13 13:29:13 +0100

    a

--- src/handlers/authHandler.ts
+++ src/handlers/authHandler.ts
@@ -22,6 +22,6 @@ export const createAccountHandler = async(request: Request, response: Response):
 		});
 		return response.status(200).send();
 	} catch(e) {
-		return response.status(403).send(e);
+		return response.status(403).send({error: e});
 	}
 }
